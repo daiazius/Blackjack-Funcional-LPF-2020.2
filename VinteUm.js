2d0d07afa204d31215558c167864ad3f06f6ba74
daiazius <psilvestre5114@gmail.com>
2021-08-18 17:53:15 +0000

    so o basico >:)

--- VinteUm.js
+++ VinteUm.js
@@ -3,22 +3,51 @@ if (typeof kotlin === 'undefined') {
 }var VinteUm = function (_, Kotlin) {
   'use strict';
   var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var removeFirst = Kotlin.kotlin.collections.removeFirst_vvxzk3$;
   var Kind_CLASS = Kotlin.Kind.CLASS;
   var shuffle = Kotlin.kotlin.collections.shuffle_vvxzk3$;
   var ArrayList_init = Kotlin.kotlin.collections.ArrayList_init_287e2$;
   function darCartas() {
     var jorje = document.getElementById('jogo');
-    var teste = document.getElementById('sla');
-    if (jorje == null || teste == null) {
+    if (jorje == null) {
       println('ihhh passou vergoinha em');
       return;
-    }teste.innerHTML = 'ola';
-    jorje = teste;
+    }var alanturing = new Baralho();
+    var sim = new Mao();
+    sim.puxarCarta_9350cv$(1, alanturing.cartas);
+    var carta = removeFirst(sim.cartasNaMao);
+    var a = carta.toString();
+    jorje.innerHTML = '\n    <link rel="stylesheet" type="text/css" href="estilos.css" media="screen"/>\n    <button onclick = "VinteUm.pedirCartas()">adiciona<\/button>    \n    ';
+  }
+  function pedirCartas() {
+    var vonNeumann = document.getElementById('jogo');
+    if (vonNeumann == null) {
+      println('ihhh passou vergoinha em');
+      return;
+    }vonNeumann.innerHTML = vonNeumann.innerHTML + '\n    <div id = "bora"><p style = "text-align: center"><\/p><\/div>\n    ';
   }
   function Carta(naipe, numero) {
     this.numero = numero;
     this.naipe = naipe;
   }
+  Carta.prototype.toString = function () {
+    var coisa = '';
+    switch (this.naipe) {
+      case 1:
+        coisa += this.numero.toString() + ' de paus';
+        break;
+      case 2:
+        coisa += this.numero.toString() + ' de ouros';
+        break;
+      case 3:
+        coisa += this.numero.toString() + ' de copas';
+        break;
+      case 4:
+        coisa += this.numero.toString() + ' de espadas';
+        break;
+    }
+    return coisa;
+  };
   Carta.$metadata$ = {
     kind: Kind_CLASS,
     simpleName: 'Carta',
@@ -50,9 +79,27 @@ if (typeof kotlin === 'undefined') {
     simpleName: 'Baralho',
     interfaces: []
   };
+  function Mao() {
+    this.cartasNaMao = ArrayList_init();
+  }
+  Mao.prototype.puxarCarta_9350cv$ = function (qntdCartas, cartas) {
+    if (qntdCartas === 0) {
+      return;
+    } else {
+      this.cartasNaMao.add_11rb$(removeFirst(cartas));
+      this.puxarCarta_9350cv$(qntdCartas - 1 | 0, cartas);
+    }
+  };
+  Mao.$metadata$ = {
+    kind: Kind_CLASS,
+    simpleName: 'Mao',
+    interfaces: []
+  };
   _.darCartas = darCartas;
+  _.pedirCartas = pedirCartas;
   _.Carta = Carta;
   _.Baralho = Baralho;
+  _.Mao = Mao;
   Kotlin.defineModule('VinteUm', _);
   return _;
 }(typeof VinteUm === 'undefined' ? {} : VinteUm, kotlin);
--- VinteUm.kt
+++ VinteUm.kt
@@ -3,24 +3,56 @@ import org.w3c.dom.*
 
 @JsName("darCartas")
 fun darCartas(){
-    var jorje = document.getElementById("jogo")
-    var teste = document.createElement("div")
 
-    if(jorje==null || teste==null){
+    val jorje = document.getElementById("jogo")
+
+    if(jorje==null){
         println("ihhh passou vergoinha em")
         return;
     }
+    
+   // val carta = Carta(2,1)
+    
+    val alanturing = Baralho()
+    val sim = Mao()
+    sim.puxarCarta(1,alanturing.cartas)
+    val carta = sim.cartasNaMao.removeFirst()
+    val a:String = carta.toString()
 
+    jorje.innerHTML = """
+    <link rel="stylesheet" type="text/css" href="estilos.css" media="screen"/>
+    <button id = "pedro" onclick = "VinteUm.pedirCartas()">adiciona</button>    
+    """
+}
+
+@JsName("pedirCartas")
+fun pedirCartas(){
+    val vonNeumann = document.getElementById("jogo")
+    if(vonNeumann==null){
+        println("ihhh passou vergoinha em")
+        return;
+    }
+    vonNeumann.innerHTML += """
+    <div id = "bora"><p style = "text-align: center"></p></div>
+    """
     
-    jorje = teste
-    
-   // val testando = Teste()
-   // testando.oi()
 }
 
 class Carta(naipe :Int, numero :Int) {
     val numero:Int = numero
     val naipe:Int = naipe
+
+    
+    override fun toString(): String{
+        var coisa:String = ""
+        when(naipe){
+            1 -> coisa += "$numero de paus"
+            2 -> coisa += "$numero de ouros"
+            3 -> coisa += "$numero de copas"
+            4 -> coisa += "$numero de espadas" 
+        }
+        return coisa
+    }
 
 }
 
@@ -29,6 +61,7 @@ class Baralho(){
     init{
         
         andaNaipe(1,13)
+        //cartas.add(4,1) //isso aq resolve a falta da última carta
         cartas.shuffle()
 
     }
@@ -51,6 +84,23 @@ class Baralho(){
             cartas.add(Carta(nNaipe,nCarta))
             andaCarta(nNaipe,nCarta-1)
         }
+    }
+}
+    
+
+class Mao(){
+    val cartasNaMao = mutableListOf<Carta>()
+
+    fun puxarCarta(qntdCartas:Int, cartas:MutableList<Carta>){
+        if(qntdCartas == 0){
+            return;
+        }
+        else{
+            //fazer com que a carta va para a mao do jogador ou do dealer
+            cartasNaMao.add(cartas.removeFirst())
+            puxarCarta(qntdCartas-1,cartas)
+        }
+
     }
 
 }
