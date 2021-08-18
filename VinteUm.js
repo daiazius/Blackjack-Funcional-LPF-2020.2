f58081255f626f8944781a30a7451dfb2b0034fc
daiazius <psilvestre5114@gmail.com>
2021-08-18 03:42:29 +0000

    codigo do dia 18

--- VinteUm.js
+++ VinteUm.js
@@ -3,18 +3,56 @@ if (typeof kotlin === 'undefined') {
 }var VinteUm = function (_, Kotlin) {
   'use strict';
   var println = Kotlin.kotlin.io.println_s8jyv4$;
-  function main() {
-  }
+  var Kind_CLASS = Kotlin.Kind.CLASS;
+  var shuffle = Kotlin.kotlin.collections.shuffle_vvxzk3$;
+  var ArrayList_init = Kotlin.kotlin.collections.ArrayList_init_287e2$;
   function darCartas() {
     var jorje = document.getElementById('jogo');
-    if (jorje == null) {
+    var teste = document.getElementById('sla');
+    if (jorje == null || teste == null) {
       println('ihhh passou vergoinha em');
       return;
-    }jorje.innerHTML = 'Voce jogou 21 e ganhou confia';
+    }teste.innerHTML = 'ola';
+    jorje = teste;
   }
-  _.main = main;
+  function Carta(naipe, numero) {
+    this.numero = numero;
+    this.naipe = naipe;
+  }
+  Carta.$metadata$ = {
+    kind: Kind_CLASS,
+    simpleName: 'Carta',
+    interfaces: []
+  };
+  function Baralho() {
+    this.cartas = ArrayList_init();
+    this.andaNaipe_vux9f0$(1, 13);
+    shuffle(this.cartas);
+  }
+  Baralho.prototype.andaNaipe_vux9f0$ = function (nNaipe, nCarta) {
+    if (nNaipe > 4) {
+      return;
+    } else {
+      this.andaCarta_vux9f0$(nNaipe, nCarta);
+      this.andaNaipe_vux9f0$(nNaipe + 1 | 0, nCarta);
+    }
+  };
+  Baralho.prototype.andaCarta_vux9f0$ = function (nNaipe, nCarta) {
+    if (nCarta === 0) {
+      return;
+    } else {
+      this.cartas.add_11rb$(new Carta(nNaipe, nCarta));
+      this.andaCarta_vux9f0$(nNaipe, nCarta - 1 | 0);
+    }
+  };
+  Baralho.$metadata$ = {
+    kind: Kind_CLASS,
+    simpleName: 'Baralho',
+    interfaces: []
+  };
   _.darCartas = darCartas;
-  main();
+  _.Carta = Carta;
+  _.Baralho = Baralho;
   Kotlin.defineModule('VinteUm', _);
   return _;
 }(typeof VinteUm === 'undefined' ? {} : VinteUm, kotlin);
--- VinteUm.kt
+++ VinteUm.kt
@@ -1,16 +1,56 @@
 import kotlinx.browser.*
 import org.w3c.dom.*
 
-fun main(){
-
-}
-
 @JsName("darCartas")
 fun darCartas(){
-    val jorje = document.getElementById("jogo")
-    if(jorje==null){
+    var jorje = document.getElementById("jogo")
+    var teste = document.createElement("div")
+
+    if(jorje==null || teste==null){
         println("ihhh passou vergoinha em")
         return;
     }
-    jorje.innerHTML += "Voce pegou 21 e ganhou confia"
+
+    
+    jorje = teste
+    
+   // val testando = Teste()
+   // testando.oi()
+}
+
+class Carta(naipe :Int, numero :Int) {
+    val numero:Int = numero
+    val naipe:Int = naipe
+
+}
+
+class Baralho(){
+    val cartas = mutableListOf<Carta>()
+    init{
+        
+        andaNaipe(1,13)
+        cartas.shuffle()
+
+    }
+ 
+    fun andaNaipe(nNaipe:Int,nCarta:Int){
+        if(nNaipe > 4){
+            return;
+        }
+        else{
+            andaCarta(nNaipe,nCarta)
+            andaNaipe(nNaipe+1,nCarta)
+        }
+    }
+
+    fun andaCarta(nNaipe:Int,nCarta:Int){
+        if(nCarta == 0){
+            return;
+        }
+        else{
+            cartas.add(Carta(nNaipe,nCarta))
+            andaCarta(nNaipe,nCarta-1)
+        }
+    }
+
 }
