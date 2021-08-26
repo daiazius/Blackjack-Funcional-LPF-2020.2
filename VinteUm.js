cb95737246bf73dd4d4f6b359810c24eeed3fdf3
daiazius <psilvestre5114@gmail.com>
2021-08-26 03:25:51 +0000

    alterações do dia 25

--- VinteUm.js
+++ VinteUm.js
@@ -11,19 +11,36 @@ if (typeof kotlin === 'undefined') {
   var ArrayList_init = Kotlin.kotlin.collections.ArrayList_init_287e2$;
   var baralhin;
   var p1;
+  var dealer;
+  var jogoAcabou;
   function darCartas() {
     p1.maoDoJogador.puxarCarta_9350cv$(2, baralhin.cartas);
+    dealer.maoDoJogador.puxarCarta_9350cv$(2, baralhin.cartas);
     var carta1 = p1.maoDoJogador.cartasNaMao.get_za3lpa$(0).toString();
     var carta2 = p1.maoDoJogador.cartasNaMao.get_za3lpa$(1).toString();
+    var cartaDoDealer1 = dealer.maoDoJogador.cartasNaMao.get_za3lpa$(0).toString();
     var pont = p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0);
+    var pontA = p1.maoDoJogador.somarOTotalcomAs_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0);
     var jorje = document.getElementById('jogo');
     if (jorje == null) {
       println('ihhh passou vergoinha em');
       return;
-    }jorje.innerHTML = '\n' + '    <link rel=' + '"' + 'stylesheet' + '"' + ' type=' + '"' + 'text/css' + '"' + ' href=' + '"' + 'estilos.css' + '"' + ' media=' + '"' + 'screen' + '"' + '/>' + '\n' + '    <div id = ' + '"' + 'cartaDealer' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + carta1 + '<\/p><\/div>' + '\n' + '    <div id = ' + '"' + 'cartaDealer' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + carta2 + '<\/p><\/div>' + '\n' + '    <div id = ' + '"' + 'carta' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + carta1 + '<\/p><\/div>' + '\n' + '    <div id = ' + '"' + 'carta' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + carta2 + '<\/p><\/div>' + '\n' + '\n' + '    <div id = ' + '"' + 'pontuacao' + '"' + '><p>Pontua\xE7\xE3o: ' + pont + '<\/p><\/div>' + '\n' + '    <div id = ' + '"' + 'pontuacaoDealer' + '"' + '><p>Pontua\xE7\xE3o: ' + pont + '<\/p><\/div>' + '\n' + '    <button id = ' + '"' + 'pedir' + '"' + ' onclick = ' + '"' + 'VinteUm.pedirCartas()' + '"' + '>Adiciona<\/button>' + '\n' + '    <button id = ' + '"' + 'manter' + '"' + '>Manter<\/button>     ' + '\n' + '    ';
+    }jorje.innerHTML = '\n    <link rel="stylesheet" type="text/css" href="estilos.css" media="screen"/>\n    <div id = "maoJogador"><\/div>\n    <div id = "maoDealer"><\/div>\n    <div id = "pontuacaoDealer"><p>Pontua\xE7\xE3o: ???<\/p><\/div>\n    <button id = "pedir" onclick = "VinteUm.pedirCartas()">Adiciona<\/button>\n    <button id = "manter" onclick = "VinteUm.manterMao()">Manter<\/button>     \n    ';
+    var jorje2 = document.getElementById('maoJogador');
+    var jorje3 = document.getElementById('maoDealer');
+    if (jorje2 == null || jorje3 == null) {
+      println('ihhh passou vergoinha em');
+      return;
+    }jorje2.innerHTML = '\n' + '    <div id = ' + '"' + 'carta' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + carta1 + '<\/p><\/div>' + '\n' + '    <div id = ' + '"' + 'carta' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + carta2 + '<\/p><\/div>' + '\n' + '    ';
+    jorje3.innerHTML = '\n' + '    <div id = ' + '"' + 'cartaDealer' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + cartaDoDealer1 + '<\/p><\/div>' + '\n' + '    <div id = ' + '"' + 'cartaDealer2' + '"' + '><\/div>' + '\n' + '    ';
+    if (p1.maoDoJogador.asNaMao) {
+      jorje.innerHTML = jorje.innerHTML + ('\n' + '        <div id = ' + '"' + 'pontuacao' + '"' + '><p>Pontua\xE7\xE3o: ' + pont + ' ' + pontA + '<\/p><\/div>' + '\n' + '        ');
+    } else {
+      jorje.innerHTML = jorje.innerHTML + ('\n' + '        <div id = ' + '"' + 'pontuacao' + '"' + '><p>Pontua\xE7\xE3o: ' + pont + '<\/p><\/div>' + '\n' + '        ');
+    }
   }
   function pedirCartas() {
-    var tmp$;
+    var tmp$, tmp$_0;
     if (baralhin.cartas.isEmpty()) {
       println('baralho vazio');
       return;
@@ -31,33 +48,135 @@ if (typeof kotlin === 'undefined') {
     var carta = last(p1.maoDoJogador.cartasNaMao).toString();
     var pont = p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0);
     var pontA = p1.maoDoJogador.somarOTotalcomAs_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0);
-    var vonNeumann = document.getElementById('jogo');
+    var vonNeumann = document.getElementById('maoJogador');
     if (vonNeumann == null) {
       println('ihhh passou vergoinha em');
       return;
     }vonNeumann.innerHTML = vonNeumann.innerHTML + ('\n' + '    <div id = ' + '"' + 'carta' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + carta + '<\/p><\/div>' + '\n' + '    ');
-    (tmp$ = document.getElementById('pontuacao')) != null ? (tmp$.innerHTML = 'Pontua\xE7\xE3o: ' + pont + ' ' + pontA) : null;
+    if (p1.maoDoJogador.asNaMao) {
+      (tmp$ = document.getElementById('pontuacao')) != null ? (tmp$.innerHTML = 'Pontua\xE7\xE3o: ' + pont + ' ' + pontA) : null;
+    } else {
+      (tmp$_0 = document.getElementById('pontuacao')) != null ? (tmp$_0.innerHTML = 'Pontua\xE7\xE3o: ' + pont) : null;
+    }
     verificinator();
   }
+  function pedirCartasDealer() {
+    if (baralhin.cartas.isEmpty()) {
+      println('baralho vazio');
+      return;
+    }dealer.maoDoJogador.puxarCarta_9350cv$(1, baralhin.cartas);
+    var carta = last(dealer.maoDoJogador.cartasNaMao).toString();
+    var vonNeumann = document.getElementById('maoDealer');
+    if (vonNeumann == null) {
+      println('ihhh passou vergoinha em');
+      return;
+    }vonNeumann.innerHTML = vonNeumann.innerHTML + ('\n' + '    <div id = ' + '"' + 'cartaDealer' + '"' + '><p id = ' + '"' + 'texto-carta' + '"' + '>' + carta + '<\/p><\/div>' + '\n' + '    ');
+  }
+  function manterMao() {
+    var tmp$, tmp$_0;
+    var cartaDoDealer2 = dealer.maoDoJogador.cartasNaMao.get_za3lpa$(1).toString();
+    var pontosJogador;
+    if (p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0) > p1.maoDoJogador.somarOTotalcomAs_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0)) {
+      pontosJogador = p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0);
+    } else {
+      pontosJogador = p1.maoDoJogador.somarOTotalcomAs_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0);
+    }
+    println('vez do dealer');
+    (tmp$ = document.getElementById('cartaDealer2')) != null ? (tmp$.innerHTML = '<p id = texto-carta>' + cartaDoDealer2 + '<\/p>') : null;
+    (tmp$_0 = document.getElementById('cartaDealer2')) != null ? (tmp$_0.id = 'cartaDealer') : null;
+    verificinatorDealer();
+    while (dealer.maoDoJogador.somarOTotal_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) < 17 && dealer.maoDoJogador.somarOTotal_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) <= pontosJogador) {
+      pedirCartasDealer();
+      verificinatorDealer();
+    }
+    if (!jogoAcabou) {
+      verificinatorDealer();
+    }}
   function verificinator() {
-    var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4;
+    var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5, tmp$_6, tmp$_7;
     if (p1.maoDoJogador.asNaMao) {
       if (p1.maoDoJogador.somarOTotalcomAs_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0) > 21) {
         println('rebentastes ou nao');
       } else if (p1.maoDoJogador.somarOTotalcomAs_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0) === 21) {
         println('vencestes');
+        (tmp$ = document.getElementById('pedir')) != null ? (tmp$.remove(), Unit) : null;
+        (tmp$_0 = document.getElementById('manter')) != null ? (tmp$_0.remove(), Unit) : null;
+        tmp$_1 = document.getElementById('jogo');
+        tmp$_1 != null ? (tmp$_1.innerHTML = (tmp$_1 != null ? tmp$_1.innerHTML : null) + '<div id = "fimDeJogo" style = "background: green;">PARABENS OTARIO<\/div>') : null;
       }}if (p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0) > 21) {
       println('rebentastes');
-      (tmp$ = document.getElementById('pedir')) != null ? (tmp$.remove(), Unit) : null;
-      (tmp$_0 = document.getElementById('manter')) != null ? (tmp$_0.remove(), Unit) : null;
-      tmp$_1 = document.getElementById('jogo');
-      tmp$_1 != null ? (tmp$_1.innerHTML = (tmp$_1 != null ? tmp$_1.innerHTML : null) + '\n            <div id = "fimDeJogo">REBENTASTES<\/div>\n            ') : null;
-    } else if (p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0) === 21) {
-      println('vencestes');
       (tmp$_2 = document.getElementById('pedir')) != null ? (tmp$_2.remove(), Unit) : null;
       (tmp$_3 = document.getElementById('manter')) != null ? (tmp$_3.remove(), Unit) : null;
       tmp$_4 = document.getElementById('jogo');
-      tmp$_4 != null ? (tmp$_4.innerHTML = (tmp$_4 != null ? tmp$_4.innerHTML : null) + '<div id = "fimDeJogo" style = "background: green;">PARABENS OTARIO<\/div>') : null;
+      tmp$_4 != null ? (tmp$_4.innerHTML = (tmp$_4 != null ? tmp$_4.innerHTML : null) + '\n            <div id = "fimDeJogo" style = "background: darkred">REBENTASTES<\/div>\n            ') : null;
+    } else if (p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0) === 21) {
+      println('vencestes');
+      (tmp$_5 = document.getElementById('pedir')) != null ? (tmp$_5.remove(), Unit) : null;
+      (tmp$_6 = document.getElementById('manter')) != null ? (tmp$_6.remove(), Unit) : null;
+      tmp$_7 = document.getElementById('jogo');
+      tmp$_7 != null ? (tmp$_7.innerHTML = (tmp$_7 != null ? tmp$_7.innerHTML : null) + '<div id = "fimDeJogo" style = "background: green;">PARABENS OTARIO<\/div>') : null;
+    }}
+  function verificinatorDealer() {
+    var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5, tmp$_6, tmp$_7, tmp$_8, tmp$_9, tmp$_10, tmp$_11, tmp$_12, tmp$_13, tmp$_14, tmp$_15, tmp$_16, tmp$_17, tmp$_18, tmp$_19, tmp$_20, tmp$_21, tmp$_22;
+    if (dealer.maoDoJogador.asNaMao) {
+      if (dealer.maoDoJogador.somarOTotalcomAs_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) > 21) {
+        println('rebentastes ou nao');
+      } else if (dealer.maoDoJogador.somarOTotalcomAs_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) === 21) {
+        println('vencestes');
+        (tmp$ = document.getElementById('pedir')) != null ? (tmp$.remove(), Unit) : null;
+        (tmp$_0 = document.getElementById('manter')) != null ? (tmp$_0.remove(), Unit) : null;
+        tmp$_1 = document.getElementById('jogo');
+        tmp$_1 != null ? (tmp$_1.innerHTML = (tmp$_1 != null ? tmp$_1.innerHTML : null) + '<div id = "fimDeJogo" style = "background: blue;">PERDEU OTARIO<\/div>') : null;
+        jogoAcabou = true;
+      } else if (dealer.maoDoJogador.somarOTotalcomAs_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) > p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0)) {
+        println('vencestes');
+        (tmp$_2 = document.getElementById('pedir')) != null ? (tmp$_2.remove(), Unit) : null;
+        (tmp$_3 = document.getElementById('manter')) != null ? (tmp$_3.remove(), Unit) : null;
+        tmp$_4 = document.getElementById('jogo');
+        tmp$_4 != null ? (tmp$_4.innerHTML = (tmp$_4 != null ? tmp$_4.innerHTML : null) + '<div id = "fimDeJogo" style = "background: yellow;">PERDEU OTARIO<\/div>') : null;
+        jogoAcabou = true;
+      } else if (dealer.maoDoJogador.somarOTotalcomAs_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) === p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0)) {
+        println('vencestes');
+        (tmp$_5 = document.getElementById('pedir')) != null ? (tmp$_5.remove(), Unit) : null;
+        (tmp$_6 = document.getElementById('manter')) != null ? (tmp$_6.remove(), Unit) : null;
+        tmp$_7 = document.getElementById('jogo');
+        tmp$_7 != null ? (tmp$_7.innerHTML = (tmp$_7 != null ? tmp$_7.innerHTML : null) + '<div id = "fimDeJogo" style = "background: green;">EMPATE OTARIO<\/div>') : null;
+        jogoAcabou = true;
+      }}if (dealer.maoDoJogador.somarOTotal_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) > 21) {
+      println('rebentastes');
+      (tmp$_8 = document.getElementById('pedir')) != null ? (tmp$_8.remove(), Unit) : null;
+      (tmp$_9 = document.getElementById('manter')) != null ? (tmp$_9.remove(), Unit) : null;
+      tmp$_10 = document.getElementById('jogo');
+      tmp$_10 != null ? (tmp$_10.innerHTML = (tmp$_10 != null ? tmp$_10.innerHTML : null) + '\n            <div id = "fimDeJogo">GANHASTES<\/div>\n            ') : null;
+      jogoAcabou = true;
+    } else if (dealer.maoDoJogador.somarOTotal_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) === 21) {
+      println('vencestes');
+      (tmp$_11 = document.getElementById('pedir')) != null ? (tmp$_11.remove(), Unit) : null;
+      (tmp$_12 = document.getElementById('manter')) != null ? (tmp$_12.remove(), Unit) : null;
+      tmp$_13 = document.getElementById('jogo');
+      tmp$_13 != null ? (tmp$_13.innerHTML = (tmp$_13 != null ? tmp$_13.innerHTML : null) + '<div id = "fimDeJogo" style = "background: brown;">PERDEU OTARIO<\/div>') : null;
+      jogoAcabou = true;
+    } else if (dealer.maoDoJogador.somarOTotal_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) > p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0)) {
+      println('vencestes');
+      (tmp$_14 = document.getElementById('pedir')) != null ? (tmp$_14.remove(), Unit) : null;
+      (tmp$_15 = document.getElementById('manter')) != null ? (tmp$_15.remove(), Unit) : null;
+      tmp$_16 = document.getElementById('jogo');
+      tmp$_16 != null ? (tmp$_16.innerHTML = (tmp$_16 != null ? tmp$_16.innerHTML : null) + '<div id = "fimDeJogo" style = "background: pink;">PERDEU OTARIO<\/div>') : null;
+      jogoAcabou = true;
+    } else if (dealer.maoDoJogador.somarOTotal_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) === p1.maoDoJogador.somarOTotal_za3lpa$(p1.maoDoJogador.cartasNaMao.size - 1 | 0)) {
+      println('vencestes');
+      (tmp$_17 = document.getElementById('pedir')) != null ? (tmp$_17.remove(), Unit) : null;
+      (tmp$_18 = document.getElementById('manter')) != null ? (tmp$_18.remove(), Unit) : null;
+      tmp$_19 = document.getElementById('jogo');
+      tmp$_19 != null ? (tmp$_19.innerHTML = (tmp$_19 != null ? tmp$_19.innerHTML : null) + '<div id = "fimDeJogo" style = "background: purple;">EMPATE OTARIO<\/div>') : null;
+      jogoAcabou = true;
+    } else if (dealer.maoDoJogador.somarOTotal_za3lpa$(dealer.maoDoJogador.cartasNaMao.size - 1 | 0) >= 17) {
+      println('vencestes');
+      (tmp$_20 = document.getElementById('pedir')) != null ? (tmp$_20.remove(), Unit) : null;
+      (tmp$_21 = document.getElementById('manter')) != null ? (tmp$_21.remove(), Unit) : null;
+      tmp$_22 = document.getElementById('jogo');
+      tmp$_22 != null ? (tmp$_22.innerHTML = (tmp$_22 != null ? tmp$_22.innerHTML : null) + '<div id = "fimDeJogo" style = "background: green;">PARABENS OTARIO<\/div>') : null;
+      jogoAcabou = true;
     }}
   function Carta(naipe, numero) {
     this.naipe = naipe;
@@ -183,15 +302,33 @@ if (typeof kotlin === 'undefined') {
       return p1;
     }
   });
+  Object.defineProperty(_, 'dealer', {
+    get: function () {
+      return dealer;
+    }
+  });
+  Object.defineProperty(_, 'jogoAcabou', {
+    get: function () {
+      return jogoAcabou;
+    },
+    set: function (value) {
+      jogoAcabou = value;
+    }
+  });
   _.darCartas = darCartas;
   _.pedirCartas = pedirCartas;
+  _.pedirCartasDealer = pedirCartasDealer;
+  _.manterMao = manterMao;
   _.verificinator = verificinator;
+  _.verificinatorDealer = verificinatorDealer;
   _.Carta = Carta;
   _.Baralho = Baralho;
   _.Mao = Mao;
   _.Jogador = Jogador;
   baralhin = new Baralho();
   p1 = new Jogador('joao');
+  dealer = new Jogador('dealer');
+  jogoAcabou = false;
   Kotlin.defineModule('VinteUm', _);
   return _;
 }(typeof VinteUm === 'undefined' ? {} : VinteUm, kotlin);
--- VinteUm.kt
+++ VinteUm.kt
@@ -4,16 +4,20 @@ import org.w3c.dom.*
 
 val baralhin = Baralho()
 val p1 = Jogador("joao")
-
+val dealer = Jogador("dealer")
+var jogoAcabou:Boolean = false
 
 @JsName("darCartas")
 fun darCartas(){
 
     p1.maoDoJogador.puxarCarta(2,baralhin.cartas)
+    dealer.maoDoJogador.puxarCarta(2,baralhin.cartas)
 
     val carta1:String = p1.maoDoJogador.cartasNaMao.get(0).toString()
     val carta2:String = p1.maoDoJogador.cartasNaMao.get(1).toString()
+    val cartaDoDealer1:String = dealer.maoDoJogador.cartasNaMao.get(0).toString()
     val pont:Int = p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size - 1)
+    val pontA:Int = p1.maoDoJogador.somarOTotalcomAs(p1.maoDoJogador.cartasNaMao.size-1)
 
     val jorje = document.getElementById("jogo")
 
@@ -24,16 +28,40 @@ fun darCartas(){
 
     jorje.innerHTML = """
     <link rel="stylesheet" type="text/css" href="estilos.css" media="screen"/>
-    <div id = "cartaDealer"><p id = "texto-carta">$carta1</p></div>
-    <div id = "cartaDealer"><p id = "texto-carta">$carta2</p></div>
+    <div id = "maoJogador"></div>
+    <div id = "maoDealer"></div>
+    <div id = "pontuacaoDealer"><p>Pontuação: ???</p></div>
+    <button id = "pedir" onclick = "VinteUm.pedirCartas()">Adiciona</button>
+    <button id = "manter" onclick = "VinteUm.manterMao()">Manter</button>     
+    """
+    val jorje2 = document.getElementById("maoJogador")
+    val jorje3 = document.getElementById("maoDealer")
+
+    if(jorje2 == null || jorje3 == null){
+        println("ihhh passou vergoinha em")
+        return;
+    }
+    
+    jorje2.innerHTML = """
     <div id = "carta"><p id = "texto-carta">$carta1</p></div>
     <div id = "carta"><p id = "texto-carta">$carta2</p></div>
-
-    <div id = "pontuacao"><p>Pontuação: $pont</p></div>
-    <div id = "pontuacaoDealer"><p>Pontuação: $pont</p></div>
-    <button id = "pedir" onclick = "VinteUm.pedirCartas()">Adiciona</button>
-    <button id = "manter">Manter</button>     
     """
+
+    jorje3.innerHTML = """
+    <div id = "cartaDealer"><p id = "texto-carta">$cartaDoDealer1</p></div>
+    <div id = "cartaDealer2"></div>
+    """
+ 
+    if(p1.maoDoJogador.asNaMao){
+        jorje.innerHTML += """
+        <div id = "pontuacao"><p>Pontuação: $pont $pontA</p></div>
+        """
+    }
+    else{
+        jorje.innerHTML += """
+        <div id = "pontuacao"><p>Pontuação: $pont</p></div>
+        """
+    }
 }
 
 @JsName("pedirCartas")
@@ -50,7 +78,7 @@ fun pedirCartas(){
     val pontA:Int = p1.maoDoJogador.somarOTotalcomAs(p1.maoDoJogador.cartasNaMao.size-1)
     
     
-    val vonNeumann = document.getElementById("jogo")
+    val vonNeumann = document.getElementById("maoJogador")
 
     if(vonNeumann==null){
         println("ihhh passou vergoinha em")
@@ -59,11 +87,65 @@ fun pedirCartas(){
     vonNeumann.innerHTML += """
     <div id = "carta"><p id = "texto-carta">$carta</p></div>
     """
-
-    document.getElementById("pontuacao")?.innerHTML = "Pontuação: $pont $pontA";
-
+    if(p1.maoDoJogador.asNaMao){
+        document.getElementById("pontuacao")?.innerHTML = "Pontuação: $pont $pontA";
+    }
+    else{
+        document.getElementById("pontuacao")?.innerHTML = "Pontuação: $pont";
+    }
     verificinator()
 
+}
+
+@JsName("pedirCartasDealer")
+fun pedirCartasDealer(){
+
+    if(baralhin.cartas.isEmpty()){
+        println("baralho vazio")
+        return;
+    }
+
+    dealer.maoDoJogador.puxarCarta(1,baralhin.cartas)
+    val carta:String = dealer.maoDoJogador.cartasNaMao.last().toString()
+    val vonNeumann = document.getElementById("maoDealer")
+    if(vonNeumann==null){
+        println("ihhh passou vergoinha em")
+        return;
+    }
+    vonNeumann.innerHTML += """
+    <div id = "cartaDealer"><p id = "texto-carta">$carta</p></div>
+    """
+
+}
+
+@JsName("manterMao")
+fun manterMao(){
+    val cartaDoDealer2:String = dealer.maoDoJogador.cartasNaMao.get(1).toString()
+    val pontosJogador:Int
+    if(p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size-1) > p1.maoDoJogador.somarOTotalcomAs(p1.maoDoJogador.cartasNaMao.size-1)){
+        pontosJogador = p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size-1)
+    }
+    else{
+        pontosJogador = p1.maoDoJogador.somarOTotalcomAs(p1.maoDoJogador.cartasNaMao.size-1)
+    }
+    println("vez do dealer")
+    document.getElementById("cartaDealer2")?.innerHTML = "<p id = texto-carta>$cartaDoDealer2</p>"
+    document.getElementById("cartaDealer2")?.id = "cartaDealer"
+    
+    verificinatorDealer()
+   // if(dealer.maoDoJogador.somarOTotal(dealer.maoDoJogador.cartasNaMao.size-1) >= dealer.maoDoJogador.somarOTotalcomAs(dealer.maoDoJogador.cartasNaMao.size-1)){
+        while(dealer.maoDoJogador.somarOTotal(dealer.maoDoJogador.cartasNaMao.size-1) < 17 && 
+        dealer.maoDoJogador.somarOTotal(dealer.maoDoJogador.cartasNaMao.size-1) <= pontosJogador){
+            pedirCartasDealer()
+            verificinatorDealer()
+        }
+      //  if(!jogoAcabou){
+      //      verificinatorDealer()
+      //  }
+ //   }
+    if(!jogoAcabou){
+            verificinatorDealer()
+        }
 }
 
 fun verificinator(){
@@ -73,6 +155,9 @@ fun verificinator(){
         }
         else if(p1.maoDoJogador.somarOTotalcomAs(p1.maoDoJogador.cartasNaMao.size-1) == 21){
         println("vencestes")
+        document.getElementById("pedir")?.remove()
+        document.getElementById("manter")?.remove()
+        document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: green;">PARABENS OTARIO</div>""" 
         }
     }
     if(p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size-1) > 21){
@@ -80,7 +165,7 @@ fun verificinator(){
             document.getElementById("pedir")?.remove()
             document.getElementById("manter")?.remove()
             document.getElementById("jogo")?.innerHTML += """
-            <div id = "fimDeJogo">REBENTASTES</div>
+            <div id = "fimDeJogo" style = "background: darkred">REBENTASTES</div>
             """
         }
     else if(p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size-1) == 21){
@@ -90,6 +175,75 @@ fun verificinator(){
         document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: green;">PARABENS OTARIO</div>""" 
     }
 
+}
+
+fun verificinatorDealer(){
+    
+    if(dealer.maoDoJogador.asNaMao){
+        if(dealer.maoDoJogador.somarOTotalcomAs(dealer.maoDoJogador.cartasNaMao.size-1) > 21){
+            println("rebentastes ou nao")
+        }
+        else if(dealer.maoDoJogador.somarOTotalcomAs(dealer.maoDoJogador.cartasNaMao.size-1) == 21){
+            println("vencestes")
+            document.getElementById("pedir")?.remove()
+            document.getElementById("manter")?.remove()
+            document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: blue;">PERDEU OTARIO</div>"""
+            jogoAcabou = true 
+        }
+        else if(dealer.maoDoJogador.somarOTotalcomAs(dealer.maoDoJogador.cartasNaMao.size-1) > p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size-1)){
+            println("vencestes")
+            document.getElementById("pedir")?.remove()
+            document.getElementById("manter")?.remove()
+            document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: yellow;">PERDEU OTARIO</div>""" 
+            jogoAcabou = true 
+        }
+        else if(dealer.maoDoJogador.somarOTotalcomAs(dealer.maoDoJogador.cartasNaMao.size-1) == p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size-1)){
+            println("vencestes")
+            document.getElementById("pedir")?.remove()
+            document.getElementById("manter")?.remove()
+            document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: green;">EMPATE OTARIO</div>""" 
+            jogoAcabou = true 
+        }
+    }
+
+        if(dealer.maoDoJogador.somarOTotal(dealer.maoDoJogador.cartasNaMao.size-1) > 21){
+            println("rebentastes")
+            document.getElementById("pedir")?.remove()
+            document.getElementById("manter")?.remove()
+            document.getElementById("jogo")?.innerHTML += """
+            <div id = "fimDeJogo">GANHASTES</div>
+            """
+            jogoAcabou = true 
+            }
+        else if(dealer.maoDoJogador.somarOTotal(dealer.maoDoJogador.cartasNaMao.size-1) == 21){
+            println("vencestes")
+            document.getElementById("pedir")?.remove()
+            document.getElementById("manter")?.remove()
+            document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: brown;">PERDEU OTARIO</div>""" 
+            jogoAcabou = true 
+        }
+        else if(dealer.maoDoJogador.somarOTotal(dealer.maoDoJogador.cartasNaMao.size-1) > p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size-1)){
+            println("vencestes")
+            document.getElementById("pedir")?.remove()
+            document.getElementById("manter")?.remove()
+            document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: pink;">PERDEU OTARIO</div>""" 
+            jogoAcabou = true 
+        }
+        else if(dealer.maoDoJogador.somarOTotal(dealer.maoDoJogador.cartasNaMao.size-1) == p1.maoDoJogador.somarOTotal(p1.maoDoJogador.cartasNaMao.size-1)){
+            println("vencestes")
+            document.getElementById("pedir")?.remove()
+            document.getElementById("manter")?.remove()
+            document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: purple;">EMPATE OTARIO</div>""" 
+            jogoAcabou = true 
+        }
+        //SITUAÇAO Q O DEALER JA TEM 17 NO TOTAL E NAO PODE MAIS PUXAR CARTA
+        else if(dealer.maoDoJogador.somarOTotal(dealer.maoDoJogador.cartasNaMao.size-1) >= 17){
+            println("vencestes")
+            document.getElementById("pedir")?.remove()
+            document.getElementById("manter")?.remove()
+            document.getElementById("jogo")?.innerHTML += """<div id = "fimDeJogo" style = "background: green;">PARABENS OTARIO</div>""" 
+            jogoAcabou = true
+        }
 }
 
 class Carta(val naipe :Int, val numero :Int) {
